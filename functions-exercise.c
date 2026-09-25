1f8af1d62c925f99c984655247f6cbdba67f864a
Aremu Fatiu <aremufatiu258@gmail.com>
2026-09-25 17:34:02 +0100

    exercise for 'finding maximum digit in a two digit number'

--- functions-exercise.c
+++ functions-exercise.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
 
-  
+void findMaximumDigit(int twoDigitNumber)
+{
+  int firstDigit, secondDigit;
+  int maximumDigit;
+  if (twoDigitNumber / 10 != 0)
+  {
+    firstDigit = twoDigitNumber / 10;
+    secondDigit = twoDigitNumber % 10;
+    if (firstDigit > secondDigit)
+    {
+      printf("The maximum number between %d and %d is: %d", firstDigit, secondDigit, firstDigit);
+    }
+    else
+    {
+      printf("The maximum number between %d and %d is: %d", firstDigit, secondDigit, secondDigit);
+    }
+  }
+  else
+  {
+    printf("Ooops, Looks like you didn't enter a 2 - digit number!");
+  }
+}
+
+int main()
+{
+  int twoDigitNumber;
+  printf("Enter a two digit number: ");
+  scanf("%d", &twoDigitNumber);
+  findMaximumDigit(twoDigitNumber);
+
   return 0;
 }
